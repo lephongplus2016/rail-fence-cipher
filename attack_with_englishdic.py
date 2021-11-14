58e0317148cc4e2e4b2b8862bed2727841465f81
lephongplus2016 <lephongplus2016@gmail.com>
2021-11-14 22:28:31 +0700

    add product cipher

--- attack_with_englishdic.py
+++ attack_with_englishdic.py
@@ -1,11 +1,12 @@
 import re 
 import random
+from h import TanCongCesar
 from rail_fence import encryptRailFence, decryptRailFence
 
 LIMITS = 50
 
 def load_words():
-    with open('words.txt') as word_file:
+    with open('words_alpha.txt') as word_file:
         valid_words = set(word_file.read().split())
 
     return valid_words
@@ -27,7 +28,7 @@ class Decrypt_withEngDic:
         return score
     
 
-    def decrypt_Rail_fence(self, ciphertext):
+    def attack_Rail_fence(self, ciphertext):
         keyRail_fence_max =  len(ciphertext) - 1
         candidates = []
         for key_candidate in range(2, keyRail_fence_max):
@@ -40,6 +41,29 @@ class Decrypt_withEngDic:
             }
 
             candidates.append(result) 
+
+        candidates.sort(key=lambda c: c['score'], reverse=True)
+        # for x in candidates:
+        #     print(x)
+        return candidates[0]
+
+    def attack_Product(self, ciphertext):
+        keyRail_fence_max =  len(ciphertext) - 1
+        candidates = []
+
+        for key_rail_fence_candidate in range(2, keyRail_fence_max):
+            decrypt_rail_fence_cipher = decryptRailFence(ciphertext, key_rail_fence_candidate)
+            for key_cesar_candidate in range(1,25):
+                plaintext_candidate = TanCongCesar(decrypt_rail_fence_cipher, key_cesar_candidate)
+                candidate_score = self.get_english_score(plaintext_candidate)
+                result = {
+                    'key_cesar': key_cesar_candidate,
+                    'key_rail_fence': key_rail_fence_candidate,
+                    'score': candidate_score,
+                    'plaintext': plaintext_candidate
+                }
+
+                candidates.append(result) 
 
         candidates.sort(key=lambda c: c['score'], reverse=True)
         # for x in candidates:
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from attack_with_englishdic import Decrypt_withEngDic
 from rail_fence import encryptRailFence, decryptRailFence
+from h import MahoaCesar, TanCongCesar
 import time
 
 def readfile(file_name):
@@ -14,6 +15,14 @@ def writefile(file_name,content):
 	with open(file_name, "w") as myfile:
 		myfile.write(content)
 
+def encryptMessageByRailFence(fileTestcase):
+	data = readfile("testcase/"+fileTestcase)
+	print("Enter rail-fence key from 2 to",(len(data) - 1))
+	RailFenceKey = int(input())
+	cipher = encryptRailFence(data,RailFenceKey)
+	file_name_dest = "cipher_of_" + fileTestcase
+	writefile("cipher_rail_fence/" + file_name_dest, cipher)
+	return file_name_dest
 
 if __name__ == "__main__":
 
@@ -21,28 +30,23 @@ if __name__ == "__main__":
 #  ke ca dau xuong dong
 
 	parser = argparse.ArgumentParser()
+
+	parser.add_argument("-m", "--method", help = "enter encrypt method")
  
 	parser.add_argument("-t", "--testcase", help = "enter testcase file")
 	
 	args = parser.parse_args()
-	if args.testcase == None:
-		print("Enter testcase file")
-	else:
+	if args.method == "railfence":
 		# encrypt rail fence
-		data = readfile("testcase/"+args.testcase)
-		print("Enter rail-fence key from 2 to",(len(data) - 1))
-		RailFenceKey = int(input())
-		cipher = encryptRailFence(data,RailFenceKey)
-		file_name_dest = "cipher_of_" + args.testcase
-		writefile("cipher_rail_fence/" + file_name_dest, cipher)
+		file_name_dest = encryptMessageByRailFence(args.testcase)
 
 		# hacking cipher
 		cipher = readfile("cipher_rail_fence/" +file_name_dest)
 		decrypt_withEngDic_Instance = Decrypt_withEngDic()
 
-		print('Decode with Rail_fence Cipher with English Dictionary:')
+		print('Attack Rail_fence Cipher with English Dictionary:')
 		start_time = time.time()
-		resultHacking = decrypt_withEngDic_Instance.decrypt_Rail_fence(cipher)
+		resultHacking = decrypt_withEngDic_Instance.attack_Rail_fence(cipher)
 		end_time = time.time()
 		print('We calculate the key:')
 		print(resultHacking['key'])
@@ -52,6 +56,38 @@ if __name__ == "__main__":
 		print('Plaintext:')
 		print(resultHacking['plaintext'])
 
+		# print('Score:')
+		# print(resultHacking['score'])
+		# print('Score 400:')
+		# print(decrypt_withEngDic_Instance.get_english_score("is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "))
+
+
 		# write file
 		file_result = 'result_' + args.testcase 
 		writefile("result_hacking_rail_fence/"+file_result, "We calculate the key of "+args.testcase+ ":\n" + str(resultHacking['key']) + "\nElapsed_time: {0}".format(elapsed_time) + "[sec]" +"\nPlaintext:\n" + resultHacking['plaintext'] )
+
+	elif args.method == "product":
+		# encrypt
+		data = readfile("testcase/"+args.testcase)
+		print("Enter cesar key from 1 to 25")
+		CesarKey = int(input())
+		cipherCesar = MahoaCesar(data, CesarKey)
+		print("Enter rail-fence key from 2 to",(len(data) - 1))
+		RailFenceKey = int(input())
+		cipherProduct = encryptRailFence(cipherCesar, RailFenceKey)  
+		print("Product cipher: ")
+		print(cipherProduct)
+
+		# decrypt
+		decryptRailFence = decryptRailFence(cipherProduct, RailFenceKey)
+		decryptCesar = TanCongCesar(decryptRailFence, CesarKey)
+		print("Decode product cipher: ")
+		print(decryptCesar)	
+		# hacking
+		print("Attack product cipher with English Dictionary:")
+		decrypt_withEngDic_Instance = Decrypt_withEngDic()
+		print(decrypt_withEngDic_Instance.attack_Product(cipherProduct))
+
+
+	else:
+		raise ValueError("Invalid method.")
